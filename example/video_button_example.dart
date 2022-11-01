144c068a107a6d4c1b0c0157e72bed97210db10e
Hamza RIzvi <96827788+hamza-rizvii@users.noreply.github.com>
2022-11-02 02:15:06 +0500

    Initiate

---
+++ example/video_button_example.dart
@@ -0,0 +1,6 @@
+import 'package:video_button/video_button.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/video_button.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library video_button;
+
+export 'src/video_button_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
